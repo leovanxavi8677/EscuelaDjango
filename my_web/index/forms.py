416aeb717cca988e6b3b1f8067e5023e3a4d312f
leovanxavi <leo8677@hotmail.com>
2019-12-11 10:23:01 -0600

    actualizando index.html y Pagina Principal

--- my_web/index/forms.py
+++ my_web/index/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class IndexForm(forms.Form):
-    usuario = forms.CharField(label="Uuario", max_length=50, required=True)
+    usuario = forms.CharField(label="Usuario", max_length=50, required=True)
     password = forms.CharField(widget=forms.PasswordInput(),
                                error_messages={'required': 'Por Favor ingrese contraseña'})
 
